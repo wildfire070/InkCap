8f904f684ce461a7e84f438c01cb06ac8148f5de
wildfire070 <lhtran07@gmail.com>
2026-09-16 22:42:17 -0400

    Add FontCacheManager stub + getFontCacheManager() for section_persistence/epub_grayscale test targets
    Section.cpp's crossink sync added a font-cache release call ahead of EPUB
    chapter layout; these two test targets needed a matching stub.

--- test/section_persistence/stubs/GfxRenderer.h
+++ test/section_persistence/stubs/GfxRenderer.h
@@ -2,8 +2,11 @@
 
 #include <cstdint>
 
+#include "FontCacheManager.h"
+
 class GfxRenderer {
  public:
   bool isSdCardFont(int) const { return false; }
   bool releaseSdCardFontForLowMemory(int, bool = false) { return false; }
+  FontCacheManager* getFontCacheManager() { return nullptr; }
 };
